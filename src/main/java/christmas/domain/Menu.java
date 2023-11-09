939b4b1dc1ab173105eb49ed21e5f7b7ca75fcae
donghyunkim <ghffu405@gmail.com>
2023-11-09 19:16:00 +0900

    feat: 메뉴리스트 찾는 메소드 추가

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -6,17 +6,35 @@ import java.util.List;
 public enum Menu {
 
     APPETIZER("애피타이저",
-            Arrays.asList(AppetizerList.MUSHROOM_SOUP, AppetizerList.TAPAS, AppetizerList.CAESAR_SALAD)),
+            Arrays.asList(MenuList.MUSHROOM_SOUP, MenuList.TAPAS, MenuList.CAESAR_SALAD)),
     MAIN("메인",
-            Arrays.asList(MainList.T_BONE_STEAK, MainList.BARBECUE_RIBS, MainList.SEAFOOD_PASTA, MainList.CHRISTMAS_PASTA)),
-    DESSERT("디저트", Arrays.asList(DessertList.CHOCOLATE_CAKE, DessertList.ICE_CREAM)),
-    DRINK("음료", Arrays.asList(DrinkList.ZERO_COKE, DrinkList.RED_WINE, DrinkList.CHAMPAGNE));
+            Arrays.asList(MenuList.T_BONE_STEAK, MenuList.BARBECUE_RIBS, MenuList.SEAFOOD_PASTA, MenuList.CHRISTMAS_PASTA)),
+    DESSERT("디저트", Arrays.asList(MenuList.CHOCOLATE_CAKE, MenuList.ICE_CREAM)),
+    DRINK("음료", Arrays.asList(MenuList.ZERO_COKE, MenuList.RED_WINE, MenuList.CHAMPAGNE));
 
     private String title;
-    private List<Object> menuList;
+    private List<MenuList> menus;
 
-    Menu(String title, List<Object> menuList) {
+    Menu(String title, List<MenuList> menus) {
         this.title = title;
-        this.menuList = menuList;
+        this.menus = menus;
     }
+
+    public static MenuList findMenu(String menuName){
+        return Arrays.stream(Menu.values())
+                .filter(m -> m.hasMenuName(MenuList.valueOf(menuName)))
+                .findAny()
+                .map(m -> MenuList.valueOf(menuName))
+                .orElseThrow(() -> new IllegalArgumentException("메뉴를 찾을 수 없습니다."));
+    }
+
+    public boolean hasMenuName(MenuList menu){
+        return menus.stream()
+                .anyMatch(m -> m == menu);
+    }
+
+
+
+
+
 }
