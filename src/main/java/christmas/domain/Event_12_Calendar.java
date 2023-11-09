bcc0d5f8efd3887cda16618476cab255ac75e42d
donghyunkim <ghffu405@gmail.com>
2023-11-09 16:56:55 +0900

    feat: title 이름 변경

--- src/main/java/christmas/domain/Event_12_Calendar.java
+++ src/main/java/christmas/domain/Event_12_Calendar.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public enum Event_12_Calendar {
 
-    CHRISTMAS_EVENT_DAYS("크리스마스 디데이",Arrays.asList("1","2","3","4","5","6","7","8","9","10","11",
+    CHRISTMAS_EVENT_DAYS("크리스마스 디데이 할인",Arrays.asList("1","2","3","4","5","6","7","8","9","10","11",
             "12","13","14","15","16","17","18","19","2O","21","22","23","24","25")),
-    WEEKDAYS("평일", Arrays.asList("4","5","6","7","11","12","13","14","18","19","20","21","25","26","27","28")),
+    WEEKDAYS("평일 할인", Arrays.asList("4","5","6","7","11","12","13","14","18","19","20","21","25","26","27","28")),
 
-    WEEKENDS("주말", Arrays.asList("1","2","8","9","15","16","22","23","29","30")),
-    STAR_DAYS("별 데이", Arrays.asList("3","10","17","24","25","31"));
+    WEEKENDS("주말 할인", Arrays.asList("1","2","8","9","15","16","22","23","29","30")),
+    STAR_DAYS("특별 할인", Arrays.asList("3","10","17","24","25","31"));
 
     private String title;
     private List<String> days;
