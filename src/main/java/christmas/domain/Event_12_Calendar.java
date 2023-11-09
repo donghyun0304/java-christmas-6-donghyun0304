5adedeae0357933dd4636ca2739cd745dd7d7b38
donghyunkim <ghffu405@gmail.com>
2023-11-09 21:06:45 +0900

    fix: 평일에 일요일 추가

--- src/main/java/christmas/domain/Event_12_Calendar.java
+++ src/main/java/christmas/domain/Event_12_Calendar.java
@@ -10,7 +10,7 @@ public enum Event_12_Calendar {
 
     CHRISTMAS_EVENT_DAYS(Constant.CHRISTMAS_DISCOUNT, Arrays.asList("1","2","3","4","5","6","7","8","9","10","11",
             "12","13","14","15","16","17","18","19","2O","21","22","23","24","25")),
-    WEEKDAYS(Constant.WEEKDAYS_DISCOUNT, Arrays.asList("4","5","6","7","11","12","13","14","18","19","20","21","25","26","27","28")),
+    WEEKDAYS(Constant.WEEKDAYS_DISCOUNT, Arrays.asList("3","4","5","6","7","10","11","12","13","14","17","18","19","20","21","24","25","26","27","28","31")),
 
     WEEKENDS(Constant.WEEKENDS_DISCOUNT, Arrays.asList("1","2","8","9","15","16","22","23","29","30")),
     STAR_DAYS(Constant.STAR_DAYS_DISCOUNT, Arrays.asList("3","10","17","24","25","31"));
