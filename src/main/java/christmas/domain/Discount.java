c83f3a2e25e2b8b8c865558df73331e1f1f9ff78
donghyunkim <ghffu405@gmail.com>
2023-11-09 20:01:31 +0900

    feat: 크리스마스 할인 메스도 추가

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -1,4 +1,31 @@
 package christmas.domain;
 
+import christmas.constant.Constant;
+
 public class Discount {
+
+    private String discountTitle;
+    private int discountPrice;
+
+    private Discount(String discountTitle, int discountPrice) {
+        this.discountTitle = discountTitle;
+        this.discountPrice = discountPrice;
+    }
+
+    public static Discount createChristmasDiscount(Event_12_Calendar calendar, String day){
+        if(Event_12_Calendar.hasDayInChristMasEventDays(day)){
+            return new Discount(calendar.getTitle(), (Integer.parseInt(day) - 1) * 100 + 1000);
+        }
+        throw new IllegalArgumentException("크리스마스 디데이 할인이 아닙니다.");
+    }
+
+//    public static Discount createWeekdaysDiscount(Event_12_Calendar calendar, String day, ){
+//        if(calendar.getTitle().equals(Constant.CHRISTMAS_DISCOUNT) && calendar.hasDay(day)){
+//            return new Discount(calendar.getTitle(), (Integer.parseInt(day) - 1) * 100 + 1000);
+//        }
+//        throw new IllegalArgumentException("크리스마스 디데이 할인이 아닙니다.");
+//    }
+
+
+
 }
