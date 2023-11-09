8ff5967f889032c64f9937de0a2db7aa9e2d235f
donghyunkim <ghffu405@gmail.com>
2023-11-09 21:42:46 +0900

    feat: 할인전 총주문금액 메소드 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,19 @@
 package christmas.domain;
 
+import java.util.List;
+
 public class Order {
+
+    private List<MenuList> menus;
+
+    public Order(List<MenuList> menus) {
+        this.menus = menus;
+    }
+
+    public int calcTotalAmountBeforeDiscount(){
+        return menus.stream()
+                .mapToInt(menu -> menu.getPrice())
+                .sum();
+    }
+
 }
