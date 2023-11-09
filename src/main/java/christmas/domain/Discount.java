759462bbd203ff359fadc8d9d1b2280607d0d023
donghyunkim <ghffu405@gmail.com>
2023-11-09 20:33:30 +0900

    feat: 평일 할인 메소드 추가

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -4,6 +4,8 @@ import christmas.constant.Constant;
 
 public class Discount {
 
+    public static final int WEEKDAYS_DISCOUNT_PRICE = 2023;
+
     private String discountTitle;
     private int discountPrice;
 
@@ -19,12 +21,14 @@ public class Discount {
         throw new IllegalArgumentException("크리스마스 디데이 할인이 아닙니다.");
     }
 
-//    public static Discount createWeekdaysDiscount(Event_12_Calendar calendar, String day, ){
-//        if(calendar.getTitle().equals(Constant.CHRISTMAS_DISCOUNT) && calendar.hasDay(day)){
-//            return new Discount(calendar.getTitle(), (Integer.parseInt(day) - 1) * 100 + 1000);
-//        }
-//        throw new IllegalArgumentException("크리스마스 디데이 할인이 아닙니다.");
-//    }
+    public static Discount createWeekdaysDiscount(Event_12_Calendar calendar, String day, MenuList menu){
+        if(Event_12_Calendar.hasWeekdayInWeekdays(day)){
+            if(Menu.hasMenuListInDessert(menu)){
+                return new Discount(Event_12_Calendar.WEEKENDS.getTitle(), WEEKDAYS_DISCOUNT_PRICE);
+            }
+        }
+        throw new IllegalArgumentException("평일할인이 아닙니다.");
+    }
 
 
     @Override
