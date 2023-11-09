4a38422e6680f7c1c8e8db5c014e6ae2e53debd7
donghyunkim <ghffu405@gmail.com>
2023-11-09 19:57:24 +0900

    feat: 크리스마스이벤트데이에 포함하는지 확인

--- src/main/java/christmas/domain/Event_12_Calendar.java
+++ src/main/java/christmas/domain/Event_12_Calendar.java
@@ -1,16 +1,19 @@
 package christmas.domain;
 
+import christmas.constant.Constant;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Event_12_Calendar {
 
-    CHRISTMAS_EVENT_DAYS("크리스마스 디데이 할인",Arrays.asList("1","2","3","4","5","6","7","8","9","10","11",
+    CHRISTMAS_EVENT_DAYS(Constant.CHRISTMAS_DISCOUNT, Arrays.asList("1","2","3","4","5","6","7","8","9","10","11",
             "12","13","14","15","16","17","18","19","2O","21","22","23","24","25")),
-    WEEKDAYS("평일 할인", Arrays.asList("4","5","6","7","11","12","13","14","18","19","20","21","25","26","27","28")),
+    WEEKDAYS(Constant.WEEKDAYS_DISCOUNT, Arrays.asList("4","5","6","7","11","12","13","14","18","19","20","21","25","26","27","28")),
 
-    WEEKENDS("주말 할인", Arrays.asList("1","2","8","9","15","16","22","23","29","30")),
-    STAR_DAYS("특별 할인", Arrays.asList("3","10","17","24","25","31"));
+    WEEKENDS(Constant.WEEKENDS_DISCOUNT, Arrays.asList("1","2","8","9","15","16","22","23","29","30")),
+    STAR_DAYS(Constant.STAR_DAYS_DISCOUNT, Arrays.asList("3","10","17","24","25","31"));
 
     private String title;
     private List<String> days;
@@ -24,16 +27,23 @@ public enum Event_12_Calendar {
         this.days = days;
     }
 
-    public static Event_12_Calendar findEvent(String day){
+    public static List<Event_12_Calendar> findEvent(String day){
         return Arrays.stream(Event_12_Calendar.values())
                 .filter(d -> d.hasDay(day))
-                .findAny()
-                .orElse(null);
+                .collect(Collectors.toList());
     }
 
     public boolean hasDay(String day){
         return days.stream()
                 .anyMatch(d -> d.equals(day));
     }
+
+    public boolean hasDayInChristMasEventDays(String day){
+        return Arrays.stream(Event_12_Calendar.values())
+                .anyMatch(d -> d==CHRISTMAS_EVENT_DAYS && d.hasDay(day));
+    }
+
+
+
 
 }
