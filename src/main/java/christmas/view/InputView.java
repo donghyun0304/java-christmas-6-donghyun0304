cc16d3af66dfd01e6f68d284d65c575f5d1eb4e5
donghyunkim <ghffu405@gmail.com>
2023-11-09 19:19:53 +0900

    feat: 날짜입력 메소드 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,12 @@
 package christmas.view;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class InputView {
+
+    public String inputDay(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return readLine();
+    }
+
 }
