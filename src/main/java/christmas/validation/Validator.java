4160f7e4fa2d8857e9bbf937b9e2b797d5f73fbd
donghyunkim <ghffu405@gmail.com>
2023-11-10 12:00:43 +0900

    feat: 검증 메소드 추가

--- src/main/java/christmas/validation/Validator.java
+++ src/main/java/christmas/validation/Validator.java
@@ -1,5 +1,13 @@
 package christmas.validation;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Validator {
 
     public static void validateOnlyNumber(String str) {
@@ -13,5 +21,29 @@ public class Validator {
             throw new IllegalArgumentException("1~31사이의 숫자가 아닙니다.");
         }
     }
+
+    public static void validateOrderForm(String str){
+        String regex = "([가-힣]+-\\d+)(,[가-힣]+-\\d+)*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("주문 형식에 맞지 않습니다.");
+        }
+    }
+
+    public static void validateDuplicate(String str) {
+        String[] menus = str.split(",");
+        HashSet<String> set = new HashSet<>();
+
+        for(String menu : menus){
+            String menuName = menu.split("-")[0];
+            if(set.contains(menuName)){
+                throw new IllegalArgumentException("중복 메뉴가 존재합니다.");
+            }
+            set.add(menuName);
+        }
+    }
+
 
 }
