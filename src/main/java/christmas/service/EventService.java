89df94bc797390cefac70a68929d7f4b6369303d
donghyunkim <ghffu405@gmail.com>
2023-11-12 14:17:36 +0900

    refactor: 코드 리팩토링

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -5,52 +5,41 @@ import christmas.domain.Event_12_Calendar;
 import christmas.domain.MenuList;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class EventService {
 
     public List<Discount> getDiscounts(List<Event_12_Calendar> events, String day, Map<MenuList, Integer> menus){
         List<Discount> discounts = new ArrayList<>();
-
         if(events.contains(Event_12_Calendar.CHRISTMAS_EVENT_DAYS)){
-            Discount christmasDiscount = Discount.createChristmasDiscount(day);
-//            System.out.println(christmasDiscount.toString());
-            discounts.add(christmasDiscount);
+            addDiscountIfNotNull(Discount.createChristmasDiscount(day), discounts);
         }
-
         if(events.contains(Event_12_Calendar.WEEKDAYS)){
-            Set<MenuList> menuLists = menus.keySet();
-            for(MenuList m : menuLists){
-                for(int i=0; i<menus.get(m); i++){
-                    Discount weekdaysDiscount = Discount.createWeekdaysDiscount(day, m);
-                    if(weekdaysDiscount != null){
-//                        System.out.println(weekdaysDiscount.toString());
-                        discounts.add(weekdaysDiscount);
-                    }
-                }
-            }
+            addDiscountsForMenus(day, menus, discounts, Discount::createWeekdaysDiscount);
         }
-
         if(events.contains(Event_12_Calendar.WEEKENDS)){
-            Set<MenuList> menuLists = menus.keySet();
-            for(MenuList m : menuLists) {
-                for(int i=0; i<menus.get(m); i++) {
-                    Discount weekendsDiscount = Discount.createWeekendsDiscount(day, m);
-                    if(weekendsDiscount != null){
-//                        System.out.println(weekendsDiscount.toString());
-                        discounts.add(weekendsDiscount);
-                    }
-                }
+            addDiscountsForMenus(day, menus, discounts, Discount::createWeekendsDiscount);
+        }
+        if(events.contains(Event_12_Calendar.STAR_DAYS)){
+            addDiscountIfNotNull(Discount.createStarDiscount(day), discounts);
+        }
+        return discounts;
+    }
+
+    private void addDiscountIfNotNull(Discount discount, List<Discount> discounts) {
+        if(discount != null){
+            discounts.add(discount);
+        }
+    }
+
+    private void addDiscountsForMenus(String day, Map<MenuList, Integer> menus, List<Discount> discounts,
+                                      BiFunction<String, MenuList, Discount> discountCreator) {
+        for(Map.Entry<MenuList, Integer> entry : menus.entrySet()){
+            MenuList menu = entry.getKey();
+            int quantity = entry.getValue();
+            for(int i = 0; i < quantity; i++){
+                addDiscountIfNotNull(discountCreator.apply(day, menu), discounts);
             }
         }
-
-        if(events.contains(Event_12_Calendar.STAR_DAYS)){
-            Discount starDiscount = Discount.createStarDiscount(day);
-//            System.out.println(starDiscount.toString());
-            discounts.add(starDiscount);
-        }
-
-//        System.out.println(discounts.toString());
-
-        return discounts;
     }
 }
